942fb6459a9323ea350e4af277a2fd415bbecb48
Colin Grodecoeur <colin.grodecoeur@360learning.com>
2022-06-01 10:11:56 +0200

    Lighten OrderItem constructor

--- typescript/src/domain/OrderItem.ts
+++ typescript/src/domain/OrderItem.ts
@@ -3,43 +3,31 @@ import Product from './Product';
 class OrderItem {
   private product: Product;
   private quantity: number;
-  private taxedAmount: number;
 
   public constructor(product: Product, quantity: number) {
     this.product = product;
     this.quantity = quantity;
-    const unitaryTax: number = Math.round(product.getPrice() / 100 * product.getCategory().getTaxPercentage() * 100) / 100;
-    const unitaryTaxedAmount: number = Math.round((product.getPrice() + unitaryTax) * 100) / 100;
-    this.taxedAmount = Math.round(unitaryTaxedAmount * quantity * 100) / 100;
   }
 
   public getProduct(): Product {
     return this.product;
   }
 
-  public setProduct(product: Product): void {
-    this.product = product;
-  }
-
   public getQuantity(): number {
     return this.quantity;
   }
 
-  public setQuantity(quantity: number): void {
-    this.quantity = quantity;
-  }
-
   public getTaxedAmount(): number {
-    return this.taxedAmount;
+    const unitaryTaxedAmount: number = Math.round((this.product.getPrice() + this.computeUnitaryTax()) * 100) / 100;
+    return Math.round(unitaryTaxedAmount * this.quantity * 100) / 100;
   }
 
-  public setTaxedAmount(taxedAmount: number): void {
-    this.taxedAmount = taxedAmount;
+  private computeUnitaryTax() {
+    return Math.round(this.product.getPrice() / 100 * this.product.getCategory().getTaxPercentage() * 100) / 100;
   }
 
   public getTax(): number {
-    const unitaryTax: number = Math.round(this.product.getPrice() / 100 * this.product.getCategory().getTaxPercentage() * 100) / 100;
-    return unitaryTax * this.quantity;
+    return this.computeUnitaryTax() * this.quantity;
   }
 }
 
