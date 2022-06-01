56f1ab9c89a747983cb2087805d60b5bcd499427
Alexis Lozano <alexis.lozano@360learning.com>
2022-06-01 09:12:52 +0200

    chore: eslint fix

--- typescript/src/domain/Category.ts
+++ typescript/src/domain/Category.ts
@@ -3,19 +3,19 @@ class Category {
   private taxPercentage: number;
 
   public getName(): string {
-      return this.name;
+    return this.name;
   }
 
   public setName(name: string): void {
-      this.name = name;
+    this.name = name;
   }
 
   public getTaxPercentage(): number {
-      return this.taxPercentage;
+    return this.taxPercentage;
   }
 
   public setTaxPercentage(taxPercentage: number) {
-      this.taxPercentage = taxPercentage;
+    this.taxPercentage = taxPercentage;
   }
 }
 
